c45f134c1685878365c929af0d25766f48b1b272
Vincy Chan <chanwanvincy@users.noreply.github.com>
2023-06-02 18:28:18 +0930

    more CSS

--- client/js/components/depot_info.js
+++ client/js/components/depot_info.js
@@ -17,8 +17,8 @@ function renderDepotInfo(depot_id){
     </section>
     <form action="" onSubmit = "renderDepotCommentList(depot_id)">
       <input type="textarea">
-    <button>Submit Comment</button>
-  </form>
+      <button>Submit Comment</button>
+    </form>
 `
 const commentsDOM = document.querySelector('#comments');
 commentsDOM.innerHTML = state.comments.map(comment => `
