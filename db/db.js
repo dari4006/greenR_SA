e06c5bcd6a8ecd1f7c184179a37ae1ff4b7377a4
mirzabaig <baig.zee@hotmail.com>
2023-06-02 10:19:09 +0930

    adding environment variables for deploy

--- db/db.js
+++ db/db.js
@@ -1,15 +1,27 @@
 const pg = require('pg')
 
-let db
-if (process.env.DB_PASSWORD) {
-    db = new pg.Pool({
-        database: "greenr_sa",
-        password: process.env.DB_PASSWORD
-    })
+// change this to your actual local database name
+const localDbName = 'greenr_sa'
+
+let db;
+if (process.env.DATABASE_URL) {
+  db = new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  })
 } else {
+  if (process.env.DEV_DB_PASSWORD) {
     db = new pg.Pool({
-        database: "greenr_sa"
+      database: localDbName,
+      password: process.env.DEV_DB_PASSWORD
     })
+  } else {
+    db = new pg.Pool({
+      database: localDbName
+    })
+  }
 }
 
 module.exports = db
