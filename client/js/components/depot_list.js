345cc33547f092b88fab9f18d961887ec193df40
wenhuixu <wenhuixu2017@outlook.com>
2023-06-02 10:12:02 +0930

    adding filter function

--- client/js/components/depot_list.js
+++ client/js/components/depot_list.js
@@ -1,14 +1,33 @@
 function renderDepotList() {
-    const depotListDOM = document.querySelector('.depot-list')
+  const depotListDOM = document.querySelector('#page')
 
-    depotListDOM.innerHTML = state.depots.map(depot => `
-    
-    <section class="depot">
-  <header>
-    <h2>${depot.depot_name}</h2>
-  </header>
-  <p>${depot.postcode}</p>
-  <p>${depot.region}</p>
-</section>
+  depotListDOM.innerHTML = state.depots.map(depot => `
+  <section class="depot" data-id='${depot.depot_id}>
+    <header>
+      <h2 onClick="renderDepotInfo(${depot.depot_id})" >${depot.depot_name}</h2>
+    </header>
+    <p>${depot.postcode}</p>
+    <p>${depot.region}</p>
+  </section>
 `).join('')
+}
+
+function renderSearch() {
+  document.querySelector('#page').innerHTML = `
+  <form class="search-bar" action="" onSubmit="renderSearchResult(event)">
+
+  <input type="text" name="postcode" placeholder="Please input your Postcode...">
+
+  <button>Search Depots</button>
+  </form>
+  `
+}
+
+function renderSearchResult(event) {
+  event.preventDefault();
+  const form = event.target;
+  const data = Object.fromEntries(new FormData(form))
+  let postcodeInput = data.postcode
+  state.depots = state.depots.filter(depot => depot.postcode == postcodeInput);
+  renderDepotList();
 }
